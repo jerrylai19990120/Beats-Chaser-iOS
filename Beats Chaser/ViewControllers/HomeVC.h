72daf33e20ac1c4a17d809004909f034014bb89a
jerry <jerrylai19990120@gmail.com>
2021-05-11 13:45:32 +0800

    home view update

--- Beats Chaser/ViewControllers/HomeVC.h
+++ Beats Chaser/ViewControllers/HomeVC.h
@@ -8,14 +8,16 @@
 #import <UIKit/UIKit.h>
 
 @interface HomeVC : UIViewController<UICollectionViewDelegate, UICollectionViewDataSource>
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *widthCons;
 
+@property (weak, nonatomic) IBOutlet UIView *underline;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (weak, nonatomic) IBOutlet UIStackView *homeBtn;
 @property (weak, nonatomic) IBOutlet UIStackView *artistsBtn;
 @property (weak, nonatomic) IBOutlet UIStackView *favoriteBtn;
+@property (assign) CGFloat xPosition;
 - (void)configureSegmentedControl:(UISegmentedControl *)segmentedControl;
-- (void)addUnderlineSegmentedControl:(UISegmentedControl *)segmentedControl;
-- (void)moveUnderlineSegmentedControl:(UISegmentedControl *)segmentedControl;
+- (void)moveUnderlineSegmentedControl;
 @end
 
--- Beats Chaser/ViewControllers/HomeVC.m
+++ Beats Chaser/ViewControllers/HomeVC.m
@@ -20,6 +20,11 @@
     self.collectionView.delegate = self;
     self.collectionView.dataSource = self;
     [self configureSegmentedControl:self.segmentedControl];
+    
+    [self.segmentedControl addTarget:self action:@selector(moveUnderlineSegmentedControl) forControlEvents:UIControlEventValueChanged];
+    self.xPosition = self.segmentedControl.layer.frame.origin.x;
+    self.widthCons.constant = (self.segmentedControl.layer.frame.size.width/2 - self.underline.layer.frame.size.width) + self.underline.layer.frame.size.width;
+    [self.underline layoutIfNeeded];
 }
 
 
@@ -47,14 +52,27 @@
     [segmentedControl setBackgroundColor:[[UIColor alloc]initWithRed:244/255 green:145/255 blue:60/255 alpha:1]];
     [segmentedControl setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]} forState:UIControlStateNormal];
     [segmentedControl setSelectedSegmentTintColor:[[UIColor alloc]initWithRed:244/255 green:145/255 blue:60/255 alpha:1]];
-}
-
-- (void)addUnderlineSegmentedControl:(UISegmentedControl *)segmentedControl{
     
 }
 
-- (void)moveUnderlineSegmentedControl:(UISegmentedControl *)segmentedControl{
+
+- (void)moveUnderlineSegmentedControl{
+    
+    if(self.segmentedControl.selectedSegmentIndex == 0){
+        [UIView animateWithDuration:0.18 animations:^{
+            CGRect rect = self.underline.layer.frame;
+            rect.origin.x = self.xPosition;
+            [self.underline setFrame:rect];
+        }];
+    }else if(self.segmentedControl.selectedSegmentIndex == 1){
+        [UIView animateWithDuration:0.18 animations:^{
+            CGRect rect = self.underline.layer.frame;
+            rect.origin.x = self.xPosition + rect.size.width;
+            [self.underline setFrame:rect];
+        }];
+    }
     
 }
+
 
 @end
